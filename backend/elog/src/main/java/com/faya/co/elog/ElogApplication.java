6a0f3b1cbcbe017d1fecab28ec7c69a34d7551f6
gfasil <fhabtegiorgis@mum.edu>
2020-05-12 12:03:56 -0500

    back end initialized

---
+++ backend/elog/src/main/java/com/faya/co/elog/ElogApplication.java
@@ -0,0 +1,13 @@
+package com.faya.co.elog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElogApplication.class, args);
+    }
+
+}
---
+++ backend/elog/src/test/java/com/faya/co/elog/ElogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faya.co.elog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
