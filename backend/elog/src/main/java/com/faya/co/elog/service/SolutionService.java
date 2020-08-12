92bb4b09a03be6b4e259713136907ce8f43bab45
gfasil <fhabtegiorgis@mum.edu>
2020-08-12 15:39:39 -0500

    solution model service class created

---
+++ backend/elog/src/main/java/com/faya/co/elog/service/SolutionService.java
@@ -0,0 +1,27 @@
+package com.faya.co.elog.service;
+
+import com.faya.co.elog.domain.Solution;
+
+import java.util.List;
+
+public class SolutionService implements CrudService<Solution> {
+    @Override
+    public List<Solution> getAll() {
+        return null;
+    }
+
+    @Override
+    public Solution add(Solution solution) {
+        return null;
+    }
+
+    @Override
+    public Solution update(Long id, Solution solution) {
+        return null;
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
